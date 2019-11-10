3dd7f7e9137cba91af0fa147d223f06b8ebc0362
sinpe <18222544@qq.com>
2019-11-10 14:38:28 +0800

    redo

---
+++ src/classes/Middleware/CORS.php
@@ -0,0 +1,221 @@
+<?php
+/*
+ * This file is part of long/guanzy.
+ *
+ * (c) Sinpe <support@sinpe.com>
+ *
+ * For the full copyright and license information, please view the "LICENSE.md"
+ * file that was distributed with this source code.
+ */
+
+namespace Sinpe\Framework\Middleware;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+/**
+ * CORS
+ * 
+ * @author sinpe <18222544@qq.com>
+ */
+class CORS implements MiddlewareInterface
+{
+    /**
+     * @var array
+     */
+    protected $options = [
+        'origin' => '*',
+        'allow_methods' => 'GET,HEAD,PUT,POST,DELETE',
+        // 'max_age' =>
+        // 'expose_headers' =>
+        // 'allow_headers' =>
+        // 'allow_credentials' =>
+    ];
+
+    /**
+     * __construct
+     *
+     * @param array $options
+     */
+    public function __construct($options = [])
+    {
+        $this->options = array_merge($this->options, $options);
+    }
+
+    /**
+     * Execute as PSR-7 double pass middleware.
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $response = $handler->handle($request);
+
+        $response = $this->setCorsHeaders($request, $response);
+
+        return $response;
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface $response
+     * @return ResponseInterface
+     */
+    public function setCorsHeaders(
+        ServerRequestInterface $request,
+        ResponseInterface $response
+    ): ResponseInterface {
+        // 
+        $response = $this->setOrigin($request, $response);
+        $response = $this->setAllowMethods($response);
+        $response = $this->setAllowCredentials($response);
+
+        if ($request->isOptions()) {
+            // $response = $this->setOrigin($request, $response);
+            $response = $this->setMaxAge($response);
+            // $response = $this->setAllowCredentials($request, $response);
+            // $response = $this->setAllowMethods($request, $response);
+            $response = $this->setAllowHeaders($request, $response);
+        } else {
+            // $this->setOrigin($request, $response);
+            $response = $this->setExposeHeaders($response);
+            // $response = $this->setAllowCredentials($request, $response);
+        }
+
+        return $response;
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface $response
+     * @return ResponseInterface
+     */
+    protected function setOrigin(
+        ServerRequestInterface $request,
+        ResponseInterface $response
+    ): ResponseInterface {
+        //
+        $origin = $this->options['origin'];
+
+        if (is_callable($origin)) {
+            // Call origin callback with request origin
+            $origin = call_user_func(
+                $origin,
+                $request->getHeaderLine('Origin')
+            );
+        }
+
+        // handle multiple allowed origins
+        if (is_array($origin)) {
+
+            $allowedOrigins = $origin;
+
+            // default to the first allowed origin
+            $origin = reset($allowedOrigins);
+
+            // but use a specific origin if there is a match
+            foreach ($allowedOrigins as $allowedOrigin) {
+                if ($allowedOrigin === $request->getHeaderLine('Origin')) {
+                    $origin = $allowedOrigin;
+                    break;
+                }
+            }
+        }
+
+        $response = $response->withHeader('Access-Control-Allow-Origin', $origin);
+
+        return $response;
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface $response
+     * @return ResponseInterface
+     */
+    protected function setExposeHeaders(ResponseInterface $response): ResponseInterface
+    {
+        // 
+        if (isset($this->options['expose_headers'])) {
+            $exposeHeaders = $this->options['expose_headers'];
+            if (is_array($exposeHeaders)) {
+                $exposeHeaders = implode(", ", $exposeHeaders);
+            }
+
+            $response = $response->withHeader('Access-Control-Expose-Headers', $exposeHeaders);
+        }
+
+        return $response;
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface $response
+     * @return ResponseInterface
+     */
+    protected function setMaxAge(ResponseInterface $response): ResponseInterface
+    {
+        if (isset($this->options['max_age'])) {
+            $response = $response->withHeader('Access-Control-Max-Age', $this->options['max_age']);
+        }
+        return $response;
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface $response
+     * @return ResponseInterface
+     */
+    protected function setAllowCredentials(ResponseInterface $response): ResponseInterface
+    {
+        if (isset($this->options['allow_credentials']) && $this->options['allow_credentials'] === true) {
+            $response = $response->withHeader('Access-Control-Allow-Credentials', 'true');
+        }
+
+        return $response;
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface $response
+     * @return ResponseInterface
+     */
+    protected function setAllowMethods(ResponseInterface $response): ResponseInterface
+    {
+
+        if (isset($this->options['allow_methods'])) {
+            $allowMethods = $this->options['allow_methods'];
+            if (is_array($allowMethods)) {
+                $allowMethods = implode(', ', $allowMethods);
+            }
+
+            $response = $response->withHeader('Access-Control-Allow-Methods', $allowMethods);
+        }
+
+        return $response;
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface $response
+     * @return ResponseInterface
+     */
+    protected function setAllowHeaders(
+        ServerRequestInterface $request,
+        ResponseInterface $response
+    ): ResponseInterface {
+        if (isset($this->options['allow_headers'])) {
+            $allowHeaders = $this->options['allow_headers'];
+            if (is_array($allowHeaders)) {
+                $allowHeaders = implode(", ", $allowHeaders);
+            }
+        } else {  // Otherwise, use request headers
+            $allowHeaders = $request->getHeaderLine('Access-Control-Request-Headers');
+        }
+
+        if (isset($allowHeaders)) {
+            $response = $response->withHeader('Access-Control-Allow-Headers', $allowHeaders);
+        }
+
+        return $response;
+    }
+}
